344f47671b4be5ebb1483a7c3cc5a50314b6ef86
socsieng <socsieng@gmail.com>
2018-03-14 18:32:42 +1100

    Call API for relationships

--- ui/src/index.js
+++ ui/src/index.js
@@ -24,119 +24,119 @@ document.addEventListener("DOMContentLoaded", function(event) {
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width / 2, height / 2));
 
-  // d3.json("http://localhost:8081/data.json", function(error, graph) {
-  //   if (error) throw error;
+  d3.json("http://localhost:5000/intelligence/relationships?transaction=200000", function(error, graph) {
+    if (error) throw error;
 
-  const graph = data;
-  graph.transactions.forEach(t => {
-    t.source = t.from;
-    t.target = t.to;
-  });
+    // const graph = data;
+    graph.transactions.forEach(t => {
+      t.source = t.from;
+      t.target = t.to;
+    });
 
-  let link = svg
-    .append("g")
-    .attr("class", "links")
-    .selectAll("line")
-    .data(graph.transactions)
-    .enter()
-    .append("line")
-    .attr("stroke-width", 8)
-    .on("click", linkClicked);
+    let link = svg
+      .append("g")
+      .attr("class", "links")
+      .selectAll("line")
+      .data(graph.transactions)
+      .enter()
+      .append("line")
+      .attr("stroke-width", 8)
+      .on("click", linkClicked);
 
-  let node = svg
-    .append("g")
-    .attr("class", "nodes")
-    .selectAll("circle")
-    .data(graph.accounts)
-    .enter()
-    .append("circle")
-    .attr("r", 20)
-    .attr("fill", function(d) {
-      return color(d.group);
-    })
-
-    .on("click", nodeClicked)
-    .call(
-      d3
-        .drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended)
-    );
-
-  function linkClicked(d, i) {
-    if (d3.event.defaultPrevented) return; // dragged
-
-    d3
-      .select(this)
-      .transition()
-      .style("stroke", "black")
-      .transition()
-      .style("stroke", "#999");
-
-    var x = (d.source.x + d.target.x) / 2;
-    var y = (d.source.y + d.target.y) / 2;
-
-    d3
-      .select("#info")
-      .style("visibility", "visible")
-      .style("top", y > window.innerHeight / 2 ? y - 20 - 70 : y + 20)
-      .style("left", x > window.innerWidth / 2 ? x - 20 - 150 : x + 20)
-      .text(`${d.description} — $${d.amount}`);
-  }
-
-  function nodeClicked(d, i) {
-    if (d3.event.defaultPrevented) return; // dragged
-
-    d3
-      .select(this)
-      .transition()
-      .style("fill", "black")
-      .attr("r", 40)
-      .transition()
+    let node = svg
+      .append("g")
+      .attr("class", "nodes")
+      .selectAll("circle")
+      .data(graph.accounts)
+      .enter()
+      .append("circle")
       .attr("r", 20)
-      .style("fill", color(d.group));
+      .attr("fill", function(d) {
+        return color(d.group);
+      })
 
-    d3
-      .select("#info")
-      .style("visibility", "visible")
-      .style("top", d.y > window.innerHeight / 2 ? d.y - 20 - 70 : d.y + 20)
-      .style("left", d.x > window.innerWidth / 2 ? d.x - 20 - 150 : d.x + 20)
-      .text(d.name);
-  }
+      .on("click", nodeClicked)
+      .call(
+        d3
+          .drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended)
+      );
 
-  node.append("title").text(function(d) {
-    return d.name;
+    function linkClicked(d, i) {
+      if (d3.event.defaultPrevented) return; // dragged
+
+      d3
+        .select(this)
+        .transition()
+        .style("stroke", "black")
+        .transition()
+        .style("stroke", "#999");
+
+      var x = (d.source.x + d.target.x) / 2;
+      var y = (d.source.y + d.target.y) / 2;
+
+      d3
+        .select("#info")
+        .style("visibility", "visible")
+        .style("top", y > window.innerHeight / 2 ? y - 20 - 70 : y + 20)
+        .style("left", x > window.innerWidth / 2 ? x - 20 - 150 : x + 20)
+        .text(`${d.description} — $${d.amount}`);
+    }
+
+    function nodeClicked(d, i) {
+      if (d3.event.defaultPrevented) return; // dragged
+
+      d3
+        .select(this)
+        .transition()
+        .style("fill", "black")
+        .attr("r", 40)
+        .transition()
+        .attr("r", 20)
+        .style("fill", color(d.group));
+
+      d3
+        .select("#info")
+        .style("visibility", "visible")
+        .style("top", d.y > window.innerHeight / 2 ? d.y - 20 - 70 : d.y + 20)
+        .style("left", d.x > window.innerWidth / 2 ? d.x - 20 - 150 : d.x + 20)
+        .text(d.name);
+    }
+
+    node.append("title").text(function(d) {
+      return d.name;
+    });
+
+    simulation.nodes(graph.accounts).on("tick", ticked);
+
+    simulation.force("link").links(graph.transactions);
+
+    function ticked() {
+      link
+        .attr("x1", function(d) {
+          return d.source.x;
+        })
+        .attr("y1", function(d) {
+          return d.source.y;
+        })
+        .attr("x2", function(d) {
+          return d.target.x;
+        })
+        .attr("y2", function(d) {
+          return d.target.y;
+        });
+
+      node
+        .attr("cx", function(d) {
+          return d.x;
+        })
+        .attr("cy", function(d) {
+          return d.y;
+        });
+    }
   });
-
-  simulation.nodes(graph.accounts).on("tick", ticked);
-
-  simulation.force("link").links(graph.transactions);
-
-  function ticked() {
-    link
-      .attr("x1", function(d) {
-        return d.source.x;
-      })
-      .attr("y1", function(d) {
-        return d.source.y;
-      })
-      .attr("x2", function(d) {
-        return d.target.x;
-      })
-      .attr("y2", function(d) {
-        return d.target.y;
-      });
-
-    node
-      .attr("cx", function(d) {
-        return d.x;
-      })
-      .attr("cy", function(d) {
-        return d.y;
-      });
-  }
-  // });
 
   function dragstarted(d) {
     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
